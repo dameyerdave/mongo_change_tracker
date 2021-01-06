b13429f38d920de3e124810d4ac6682822195e3f
dameyerdave <dameyerdave@gmail.com>
2021-01-06 10:53:23 +0100

    read env values from .env

--- mct.py
+++ mct.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
 
+import atexit
+import logging
+import traceback
 from os import environ
 from sys import exit, stdout
+
+from dotenv import load_dotenv
+from flask import request
 from flask_api import FlaskAPI
-from flask_script import Manager, Server
 from flask_cors import CORS
+from flask_script import Manager, Server
+from friendlylog import colored_logger as log
+from mongoengine.queryset.visitor import Q
 from tendo import singleton
 from tendo.singleton import SingleInstanceException
-from modules.helper import colorize_werkzeug
-from modules.decorators import queryset_respose
-from mongoengine.queryset.visitor import Q
-import atexit
-import traceback
 
-import logging
-from friendlylog import colored_logger as log
+from modules.decorators import queryset_respose
+from modules.helper import colorize_werkzeug
 
 # Logging
 logging.basicConfig(level=logging.DEBUG)
 colorize_werkzeug()
 
 __dev__ = environ['FLASK_ENV'] == 'development' or environ['DEBUG'] == 'True'
-log.info(f"Development node is '{__dev__}'")
+log.info(f"Development mode is '{__dev__}'")
 
 # make sure only one instance is running
 if not __dev__:
@@ -58,7 +61,7 @@ def get_changes_coll(db, coll):
 @app.route('/changes/<db>/<coll>/<doc_id>/', methods=['GET'])
 def get_changes_doc_id(db, coll, doc_id):
     from models.change import Change
-    return Change.get_changes(db, coll, doc_id)
+    return Change.get_changes(db, coll, doc_id, request.args)
 
 
 @app.route('/changes/<db>/<coll>/<doc_id>/<field>/', methods=['GET'])
@@ -71,14 +74,25 @@ def get_changes_field(db, coll, doc_id, field):
 # Initialization methods
 def connectDb():
     from mongoengine import connect
-    connect('icarus', host=environ.get('CT_MONGO_DATABASE_URI'))
+    connect(
+        db=environ.get('CT_DATABASE'),
+        host=environ.get('CT_DATABASE_HOST'),
+        port=int(environ.get('CT_DATABASE_PORT')),
+        username=environ.get('CT_DATABASE_USERNAME'),
+        password=environ.get('CT_DATABASE_PASSWORD'),
+        authentication_source=environ.get('CT_AUTH_DATABASE'))
+
+
+def __get_observe_database_uri():
+    return 'mongodb://{CT_OBSERVE_USERNAME}:{CT_OBSERVE_PASSWORD}@{CT_OBSERVE_HOST}:{CT_OBSERVE_DATABASE_PORT}/{CT_OBSERVE_DATABASE}?authSource={CT_OBSERVE_AUTH_DATABASE}'.format(**environ)
 
 
 def initChangeTracker():
     log.debug("Initialze change tracker")
     from modules.change_tracker import ChangeTracker
-    changeTracker = ChangeTracker(environ.get('CT_MONGO_DATABASE_URI'),
-                                  [coll.strip() for coll in environ.get('CT_COLLECTIONS').split(',')],
+    changeTracker = ChangeTracker(__get_observe_database_uri(),
+                                  [coll.strip() for coll in environ.get(
+                                      'CT_COLLECTIONS').split(',')],
                                   [field.strip() for field in environ.get('CT_FIELDS').split(',')])
     changeTracker.start()
 
@@ -97,6 +111,7 @@ manager.add_command('runserver', server)
 
 if __name__ == "__main__":
     try:
+        load_dotenv()
         with app.app_context():
             connectDb()
             if not __dev__:
